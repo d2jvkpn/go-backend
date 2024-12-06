d0d516ecb1e7c794c2eb26d101f89180ce1ec57c
d2jvkpn <d2jvkpn@noreplay.local>
2024-12-06 20:49:37 +0800

    ...

--- deployments/_prod.k8s.sh
+++ deployments/_prod.k8s.sh
@@ -3,16 +3,23 @@ set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 exit
+kubectl get ns
+
+kubectl config get-contexts
+kubectl config current-context
+
+kubectl config use-context k8s-prod --namespace=prod
+kubectl config set-context --current --namespace=prod
 
 #### ingress tls(https)
-kubectl create secret tls k8s.domain --key k8s.domain.key --cert k8s.domain.cre
+kubectl create secret tls k8s.domain --key k8s.domain.key --cert k8s.domain.pem
 # domains: *.k8s.domain
 
 kubectl -n prod get secret/k8s.domain
 
 #### create secret and ingress-https
 kubectl create secret tls k8s.domain \
-  --key k8s.domain.key --cert k8s.domain.cer \
+  --key k8s.domain.key --cert k8s.domain.pem \
   --dry-run=client -o yaml |
   kubectl apply -f -
 
